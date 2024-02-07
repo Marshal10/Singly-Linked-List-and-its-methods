cbd7d86437974a8799cd223e35a3ef805bc6a0e7
Marshal10 <marshalmaleane123@gmail.com>
2024-02-07 21:44:50 +0530

    Add a node to the beginning

--- main.py
+++ main.py
@@ -28,11 +28,25 @@ class LinkedList:
             self.tail.next=new_node
             self.tail=new_node
         self.length+=1
+        
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+        
                         
         
 linked_list=LinkedList()
-linked_list.append(10)
-linked_list.append(20)
-linked_list.append(30)
+# linked_list.append(10)
+# linked_list.append(20)
+# linked_list.append(30)
+linked_list.prepend(40)
+linked_list.prepend(10)
+linked_list.append(50)
 
 print(linked_list)
